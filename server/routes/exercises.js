dfaf635d7ba05b5ee7cafb0c166bb8a7e32b7950
EMP-Kritazya <kritazya.upreti@uta.edu>
2026-07-27 22:40:29 -0500

    Update: Moved functionality in controller and patched new api endpoints

--- server/routes/exercises.js
+++ server/routes/exercises.js
@@ -1,36 +1,18 @@
 import { Router } from "express";
-import { pool } from "../config/database.js";
+import {
+  getAllExercises,
+  getIndividualExercise,
+  createExercise,
+  updateExercise,
+  deleteExercise,
+} from "../controllers/exercises.js";
 
 const router = Router();
 
-// Get all exercises
-router.get("/", async (req, res) => {
-  try {
-    const results = await pool.query(
-      `SELECT * FROM exercises ORDER BY exercise_id ASC`,
-    );
-    res.status(200).json(results.rows);
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-    });
-  }
-});
-
-// Individual Exercise
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const result = await pool.query("SELECT * FROM exercises WHERE id = $1", [
-      id,
-    ]);
-    if (result.rows.length === 0) {
-      return res.status(404).json({ message: "Exercise not found" });
-    }
-    res.status(200).json(result.rows[0]);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.get("/", getAllExercises);
+router.get("/:id", getIndividualExercise);
+router.post("/", createExercise);
+router.patch("/:id", updateExercise);
+router.delete("/:id", deleteExercise);
 
 export default router;
