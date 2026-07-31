14b0b80d1acab100d9ca1cabea1d1d54a50a902b
Vy Pham <anhvyphamngoc@gmail.com>
2026-07-31 15:50:34 -0700

    Added controller function to retrieve user workout history and route

--- server/routes/workoutSessions.js
+++ server/routes/workoutSessions.js
@@ -5,14 +5,17 @@ import {
   createSession,
   updateSession,
   deleteSession,
+  getUserWorkoutHistory
 } from "../controllers/workoutSessions.js";
 
 const router = Router();
 
+router.get("/user/:user_id/history", getUserWorkoutHistory);
 router.get("/", getAllSessions);
 router.get("/:id", getIndividualSession);
 router.post("/", createSession);
 router.patch("/:id", updateSession); // mark complete lives here
 router.delete("/:id", deleteSession);
+
 
 export default router;
